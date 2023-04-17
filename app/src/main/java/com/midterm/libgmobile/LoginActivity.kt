31af2bdcb1cf1be6254690da2c49d8701493bc91
Gin <hoangphuc442266@gmail.com>
2023-04-18 02:09:07 +0700

    ket noi firebase

--- app/src/main/java/com/midterm/libgmobile/LoginActivity.kt
+++ app/src/main/java/com/midterm/libgmobile/LoginActivity.kt
@@ -1,9 +1,15 @@
 package com.midterm.libgmobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.midterm.libgmobile.databinding.ActivityLoginBinding
+import com.midterm.libgmobile.model.UserModel
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,26 +18,48 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //set onChangeListener for checkbox
-        binding.cBHiddenPassword.setOnCheckedChangeListener { _, isChecked ->
+        // check box hidden password
+        binding.cbHiddenPassword.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                // show password
-                binding.eTPasswordLogin.transformationMethod = PasswordTransformationMethod.getInstance()
+                binding.etPasswordLogin.transformationMethod = HideReturnsTransformationMethod.getInstance()
             } else {
-                // hide password
-                binding.eTPasswordLogin.transformationMethod = null
+                binding.etPasswordLogin.transformationMethod = PasswordTransformationMethod.getInstance()
             }
         }
 
-        // set onclick listener
-        binding.btnLogin.setOnClickListener {
-            // get text from edittext
-            val username = binding.eTEmailLogin.text.toString()
-            val password = binding.eTPasswordLogin.text.toString()
+        // button login
+        binding.button.setOnClickListener {
+//            val email = binding.etEmailLogin.text.toString()
+//            val password = binding.etPasswordLogin.text.toString()
+//            if (email.isNotEmpty() && password.isNotEmpty()) {
+//
+//            }
+            //get reference to database
+            val rfDB: DatabaseReference = Firebase.database.reference
+            // create user object
+            val user = UserModel(
+                id = "Test0",
+                name = "Test",
+                email = "Test",
+                password = "Test",
+                phone = "Test",
+                gender = "Test",
+                avatar = "Test",
+                isLogin = true
+            )
+            // add user to database
+            rfDB.child("users").child(user.id).setValue(user).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    // notify user
+                    Toast.makeText(this, getString(R.string.dang_nhap_thanh_cong), Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.dang_nhap_that_bai), Toast.LENGTH_SHORT).show()
+                }
+            }
         }
-
 
     }
 }
---
+++ app/src/main/java/com/midterm/libgmobile/model/UserModel.kt
@@ -0,0 +1,13 @@
+package com.midterm.libgmobile.model
+
+class UserModel(
+    var id: String = "",
+    var name: String = "",
+    var email: String = "",
+    var password: String = "",
+    var phone: String = "",
+    var gender: String = "",
+    var avatar: String = "",
+    var isLogin: Boolean = false
+) {
+}
