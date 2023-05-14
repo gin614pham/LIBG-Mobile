479c25de727946e5f0cf26cbb513c95bc7c988cf
Gin <hoangphuc442266@gmail.com>
2023-05-14 15:53:22 +0700

    update user info and image picker

--- app/src/main/java/com/midterm/libgmobile/AccountActivity.kt
+++ app/src/main/java/com/midterm/libgmobile/AccountActivity.kt
@@ -6,18 +6,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
+import android.util.Log
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.midterm.libgmobile.databinding.ActivityAccountBinding
+import com.midterm.libgmobile.databinding.LayoutUserInfoBinding
 import com.midterm.libgmobile.model.UserModel
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlin.math.log
 
 class AccountActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAccountBinding
     private var user = UserModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
-        binding = ActivityAccountBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         getUser()
         checkLogin()
         setData()
@@ -26,13 +29,31 @@ class AccountActivity : AppCompatActivity() {
         content.setSpan(
             UnderlineSpan(),0,text.length,0
         )
-        binding.tvChangePassword.text = content
-        // set OnClickListener for TextView tvChangePassword
-        binding.tvChangePassword.setOnClickListener {
-            println("change password")
+        val pickerMedia = registerForActivityResult(
+            ActivityResultContracts.PickVisualMedia()
+        ) { uri ->
+            if (uri != null) {
+                Log.d("uri", uri.toString())
+            } else {
+                Log.d("uri", "null")
+            }
+
+        }
+        val changeAvatar = findViewById<CircleImageView>(R.id.imageView_avatar_user)
+        changeAvatar.setOnClickListener() {
+            // register a photo picker in single mode
+            pickerMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
+//        binding.tvChangePassword.text = content
+//        // set OnClickListener for TextView tvChangePassword
+//        binding.tvChangePassword.setOnClickListener {
+//            println("change password")
+//        }
+
     }
+
+
 
     // get data from bundle
     private fun getUser(){
@@ -50,10 +71,10 @@ class AccountActivity : AppCompatActivity() {
     // fun set data to layout
     @SuppressLint("SetTextI18n")
     private fun setData(){
-        binding.tvId.text = getString(R.string.ma_sv) +": "+ user.id
-        binding.tvEmail.text = getString(R.string.email) +": "+ user.email
-        binding.tvName.text = getString(R.string.ten) +": "+ user.name
-        binding.tvPhone.text = getString(R.string.sdt) +": "+ user.phone
-        binding.tvGender.text = getString(R.string.gioi_tinh) +": "+ user.gender
+//        binding.tvId.text = getString(R.string.ma_sv) +": "+ user.id
+//        binding.tvEmail.text = getString(R.string.email) +": "+ user.email
+//        binding.tvName.text = getString(R.string.ten) +": "+ user.name
+//        binding.tvPhone.text = getString(R.string.sdt) +": "+ user.phone
+//        binding.tvGender.text = getString(R.string.gioi_tinh) +": "+ user.gender
     }
 }
